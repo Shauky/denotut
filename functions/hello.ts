f376d64dd706a8192fee3b835fd727a2c875c9dd
Shauky <shaukathmohamed@gmail.com>
2020-10-14 05:47:37 +0500

    testing functions p2

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,6 +1,6 @@
-import { type-detect } from "https://deno.land/x/type_detect@v4.0.8/test/index.js";
+import { typeDetect } from "https://deno.land/x/type_detect@v4.0.8/test/index.js";
 
-const type = type-detect;
+const type = typeDetect;
 
 assert(type([]) === 'Array');
 
